5e161a43e980bc9a62f17dd795dc871780a3c991
ArthurHCL <619154573@qq.com>
2019-07-27 10:42:46 +0800

    code initial commit

---
+++ bin_firmware_decryption_release_tool/bin_firmware_decryption_and_download.h
@@ -0,0 +1,20 @@
+#ifndef _BIN_FIRMWARE_DECRYPTION_AND_DOWNLOAD_H_
+#define _BIN_FIRMWARE_DECRYPTION_AND_DOWNLOAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+unsigned char Firmware_Update_Process_Bar_Get(void);
+
+int bin_firmware_decryption_and_download(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
